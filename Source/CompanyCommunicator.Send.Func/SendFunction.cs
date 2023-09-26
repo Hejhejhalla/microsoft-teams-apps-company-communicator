8e65ab4a02609e1b8103a36fe7b738c0da6b65f4
Hejhejhalla <145015339+Hejhejhalla@users.noreply.github.com>
2023-09-26 10:33:15 +0200

    Update SendFunction.cs

--- Source/CompanyCommunicator.Send.Func/SendFunction.cs
+++ Source/CompanyCommunicator.Send.Func/SendFunction.cs
@@ -297,6 +297,7 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Send.Func
                 ContentType = AdaptiveCardContentType,
                 Content = JsonConvert.DeserializeObject(jsonAC),
             };
+            log.LogInformation("Här är jsonAC.Content  " + adaptiveCardAttachment.Content);
             log.LogInformation("Här är jsonAC  " + jsonAC);
             return MessageFactory.Attachment(adaptiveCardAttachment);
         }
