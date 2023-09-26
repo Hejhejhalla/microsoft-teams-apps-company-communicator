89674845a029c3c8290145675a95efbadc0a6448
Hejhejhalla <145015339+Hejhejhalla@users.noreply.github.com>
2023-09-26 13:10:28 +0200

    Update SendFunction.cs

--- Source/CompanyCommunicator.Send.Func/SendFunction.cs
+++ Source/CompanyCommunicator.Send.Func/SendFunction.cs
@@ -291,11 +291,11 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Send.Func
             jsonAC = jsonAC.Replace("[ID]", message.NotificationId);
             jsonAC = jsonAC.Replace("[KEY]", message.RecipientData.RecipientId);
             jsonAC = this.GetButtonTrackingUrl(jsonAC, message.NotificationId, message.RecipientData.RecipientId);
-
+            this.messagetitle = this.GetMessageTitle(jsonAC);
             var adaptiveCardAttachment = new Attachment()
             {
                 ContentType = AdaptiveCardContentType,
-                Content = JsonConvert.DeserializeObject(notification.Content),
+                Content = JsonConvert.DeserializeObject(jsonAC),
             };
 
             return MessageFactory.Attachment(adaptiveCardAttachment);
